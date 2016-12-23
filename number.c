509b3645a46ab480772fc4906db73ef64a17defb
neelakannan <neelakannan19@gmail.com>
2016-12-23 00:57:15 -0800

    Add files via upload

---
+++ number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("\n number is positive");
+}
+if(a<0)
+
+printf("\n number is negative");
+
+if(a==0)
+printf("\n number is zero");
+return 0;
+}
