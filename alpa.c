c7f1158f8baf513ced238356918f0fb5ba61735d
neelakannan <neelakannan19@gmail.com>
2016-12-23 01:17:58 -0800

    Add files via upload

---
+++ alpa.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    char ch;
+    printf("\n enter character");
+    scanf("%c",&ch);
+    if(isalpha(ch))
+    {
+
+        printf("\n it is alphabet");
+    }
+        else
+            printf("\n it is not a alphabet ");
+
+    }
+
